2973cbba643d5ad26b5b25dc7fa23e9c430fac0d
Alif Wibowo <alif.likes.coding@gmail.com>
2025-03-31 04:32:04 +0700

    asfdasd

---
+++ src/app/data/data-home.js
@@ -0,0 +1,22 @@
+export const homePhrase = {
+  en: "I'm Alif, what are you looking for?",
+  id: "Saya Alif, anda mencari apa?"
+}
+
+export const homeButtons = [
+  {
+    en: "what i do",
+    id: "yang saya lakukan",
+    route: "/skills",
+  },
+  {
+    en: "projects",
+    id: "proyek",
+    route: "/projects",
+  },
+  {
+    en: "who am i",
+    id: "siapa saya",
+    route: "/about"
+  }
+];
--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,31 @@
 import Typewriter from "./components/Typewriter";
 import FramerMotionWrapper from "./components/FramerMotionWrapper";
 import { AnimatePresence } from "framer-motion";
+import CustomButton from "./components/CustomButton";
+import { homeButtons } from "./data/data-home";
 
 export default function Home() {
   return (
     <AnimatePresence>
       <div className="h-screen flex flex-col items-center justify-center">
         <FramerMotionWrapper duration={1.5}>
-          <div className="flex flex-col items-center">
-            <h1 className="font-semibold text-8xl custom-shadow">
+          <div className="flex flex-col items-center text-center gap-2">
+            <h1 className="font-semibold text-6xl sm:text-8xl custom-shadow">
               <Typewriter text="Hi !" speed={100} />
             </h1>
-            <p className="font-light text-4xl custom-shadow">
+            <p className="font-light text-2xl sm:text-4xl custom-shadow">
               I'm Alif, what are you looking for?
             </p>
+            <div className="flex flex-wrap gap-3 mt-4 justify-center">
+              {homeButtons.map((button, index) => (
+                <CustomButton
+                  key={index}
+                  text={button.en}
+                  type="link"
+                  link={button.route}
+                />
+              ))}
+            </div>
           </div>
         </FramerMotionWrapper>
       </div>
